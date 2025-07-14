cd97e8b40295011561c64ab7538199d46d3526f3
teaminmueble <hectorr.ccf@gmail.com>
2025-07-14 14:04:28 -0400

    Delete App.jsx

--- App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-white text-gray-900">
-      <h1 className="text-4xl font-bold text-center mt-10">Bienvenido a Team Inmueble</h1>
-    </div>
-  );
-}
